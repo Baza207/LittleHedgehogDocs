024cf267759dc2712f232e2bccc3ce483179168b
James Barrow <baza207@me.com>
2015-02-09 23:39:54 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#
+# mian.py
+# LittleHedgehogDocs
+#
+# Created by James Barrow on 09/02/2015.
+# Copyright (c) 2015 James Barrow - Pig on a Hill Productions. All rights reserved.
+#
+
+#!/usr/bin/env python
+
+
