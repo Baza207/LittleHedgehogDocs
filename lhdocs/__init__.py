bc740b757da670cf06d9f0dcfcb98f1c778a882d
James Barrow <baza207@me.com>
2017-01-30 00:49:12 +0100

    Bumped to 0.1.1 and updated the copyright.

--- lhdocs/__init__.py
+++ lhdocs/__init__.py
@@ -8,12 +8,13 @@
 #
 
 # !/usr/bin/env python
+# flake8: noqa
 
 __title__ = 'Little Hedgehog Docs'
-__version__ = '0.1'
+__version__ = '0.1.1'
 __author__ = 'James Barrow'
 __license__ = 'MIT'
-__copyright__ = 'Copyright (c) 2015 James Barrow - Pig on a Hill Productions'
+__copyright__ = 'Copyright (c) 2015-2017 James Barrow - Pig on a Hill Productions'
 
 # Version synonym
 VERSION = __version__
